89c29d9a2a45ed383dd0c1b2d2c685c3982dde0a
MideJoy Akinleye <joyintech50@gmail.com>
2026-01-17 15:03:32 +0100

    navbar, new font etc

---
+++ script.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector('.sidebar')
+
+const showSidebar = () => {
+    sidebar.style.display = 'flex'
+}
+
+const closeSidebar = () => {
+    sidebar.style.display = 'none'
+}
